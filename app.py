22951313bf60eec8bed2d3743a5e02203473a4f7
Katrisbug <bonetkatrina@gmail.com>
2026-05-28 16:17:51 -0300

    adição do html e css

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
+
 import paho.mqtt.client as mqtt
 
 app = FastAPI()
 
-broker = "broker.hivemq.com"
+# CONFIGURAÇÃO MQTT
+broker = "192.168.0.65"
+porta = 1883
 topico = "PROJ/ECOSSISTEMA"
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-client.connect(broker, 1883)
+client.connect(broker, porta)
 
+# PASTAS
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+# PÁGINA PRINCIPAL
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
 
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
 
+
+# PÁGINA OUTRO BROKER
+@app.get("/outrobroker", response_class=HTMLResponse)
+async def outrobroker(request: Request):
+
+    return templates.TemplateResponse(
+        request=request,
+        name="outro_broker.html"
+    )
+
+
+# LIGAR LED
 @app.post("/ligar")
 def ligar():
 
-    client.publish(topico, "ligado")
+    client.publish(topico, "ON")
 
-    return {"status": "ligado"}
+    return {"mensagem": "LED ligado"}
 
+
+# DESLIGAR LED
 @app.post("/desligar")
 def desligar():
 
-    client.publish(topico, "desligado")
+    client.publish(topico, "OFF")
 
-    return {"status": "desligado"}
+    return {"mensagem": "LED desligado"}
